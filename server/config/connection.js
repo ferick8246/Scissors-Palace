c3697ceafdb37ae892bbcf8eb98ae86bf6fae477
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-31 13:25:23 -0500

    added the packages and scripts

---
+++ server/config/connection.js
@@ -0,0 +1,16 @@
+// imports
+const mongoose = require('mongoose')
+
+
+
+// connects to mongodb
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/scissors-palace', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+
+// exports
+module.exports = mongoose.connect
--- server/server.js
+++ server/server.js
@@ -1,3 +1,41 @@
+// imports
 const express = require('express')
 const path = require('path')
 const db = require('./config/connection')
+const { ApolloServer } = require('apollo-server-express')
+const { typeDefs, resolvers } = require('./schemas')
+const app = express()
+const PORT = process.env.PORT || 3001
+const server = new ApolloServer({
+    
+    typeDefs,
+    resolvers
+})
+
+
+// middleware
+server.applyMiddleware({ app })
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json)
+
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static(path.join(__dirname, '..client/build')))
+}
+
+
+app.get('*', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, '../client/build.index.html'))
+})
+
+
+// message that the port is up and running
+db.once('open', () => { 
+
+    app.listen(PORT, () => {
+        
+        console.log(`You are now up and running on port: ${PORT}!`)
+        console.log(`GraphQL can be accessed from http://localhost:${PORT}${server.graphqlPath}`)
+    }) 
+})
