74e81d318a0dcc79b844e90558f5119e16d0f66c
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-07-31 13:36:33 -0500

    added more server file

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('');
+
+const resolvers = {
+
+
+
+
+
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,14 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+
+
+
+
+
+
+
+`;
+
+
+module.exports = typeDefs;
