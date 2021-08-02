a36c15c0d1f2db9d5693079d8b74c514db00a832
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-08-02 02:36:56 -0500

    added basic files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Cart from './components/cart'
+import navBar from 'navBar';
+import aboutUs from 'aboutUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <navBar />
     </div>
   );
 }
