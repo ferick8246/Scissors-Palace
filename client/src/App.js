e32315eb841294f18b4021c211b40703f4744723
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-08-03 20:07:31 -0500

    added css:

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import './App.css';
+import './index.css';
 import Cart from './components/Cart'
 import NavBar from './components/NavBar';
 import AboutUs from './components/AboutUs'
