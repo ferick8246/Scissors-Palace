0e61644346e36a6ec6df83778d6ac426e561c03f
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-08-04 21:26:56 -0500

    trying to get the hamboorger working..do not merge this

---
+++ client/src/script.js
@@ -0,0 +1,7 @@
+// code for hamburger drop down
+document.getElementById("hamburger").onclick = function toggleMenu() {
+    const navToggle = document.getElementsByClassName("toggle");
+    for (let i = 0; i < navToggle.length; i++) {
+      navToggle.item(i).classList.toggle("hidden");
+    }
+  };
