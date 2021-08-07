8eda44091dda353cf7723e59e0ce1514b13cad92
Erick Flores <ferick8246@gmail.com>
2021-08-07 10:26:44 -0500

    shop page

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useStoreContext } from '../../utils/GlobalState'
+import { useStoreContext } from '../utils/GlobalState'
 
 function Shop() {
     const [state, dispatch] = useStoreContext()
