03d9bc984322d83d41d0f4404a2e7ff9fcbcd93a
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-08-07 10:38:58 -0500

    apollo requirements

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 
+// Apollo
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 // component imports:
 import Header from './components/Header';
 import Cart from './components/Cart'
@@ -11,9 +20,29 @@ import Contact from './components/Contact';
 import Shop from './components/Shop';
 import Footer from './components/Footer';
 
+// Middleware
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    // <ApolloProvider client={client}>
+    <ApolloProvider client={client}>
         <Router>
         <NavBar />
         <Header />
@@ -29,7 +58,7 @@ function App() {
           </Route>
         <Footer />
       </Router>
-    // </ApolloProvider>
+    </ApolloProvider>
   );
 }
 
