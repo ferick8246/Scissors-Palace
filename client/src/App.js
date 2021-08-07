27146d64284626f3c99bb4e5c062b46b5faac95e
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-08-07 14:07:00 -0500

    finished getting graphql connected

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
 import Shop from './components/Shop';
 import Footer from './components/Footer';
+import { StoreProvider } from './utils/GlobalState';
 
 // Middleware
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
@@ -48,19 +49,23 @@ function App() {
   return (
     <ApolloProvider client={client}>
         <Router>
-        <NavBar />
-        <Header />
-        <Cart />
-          <Route exact path="/about">
-            <AboutUs />
-          </Route>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          <Route exact path="/shop">
-            <Shop />
-          </Route>
-        <Footer />
+          <div>
+            <StoreProvider>
+              <NavBar />
+              <Header />
+              <Cart />
+                <Route exact path="/about">
+                  <AboutUs />
+                </Route>
+                <Route exact path="/contact">
+                  <Contact />
+                </Route>
+                <Route exact path="/shop">
+                  <Shop  />
+                </Route>
+              <Footer />
+            </StoreProvider>
+          </div>
       </Router>
     </ApolloProvider>
   );
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import { useStoreContext } from '../utils/GlobalState'
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../utils/actions'
+import { idbPromise } from '../utils/helpers'
+import { pluralize } from '../utils/helpers'
 
-function Product() {
+
+function Product(item) {
     const [state, dispatch] = useStoreContext()
 
     const {
@@ -10,7 +15,7 @@ function Product() {
         _id,
         price,
         quantity
-    }   = item;
+    }   = item
 
     const { cart } = state
 
@@ -44,6 +49,7 @@ function Product() {
             </Link>
             <div>
                 <div>{ quantity } { pluralize('item', quantity) } in stock</div>
+                <span>${price}</span>
             </div>
             <button onClick={addToCart}>Add to Cart</button>
         </div>
