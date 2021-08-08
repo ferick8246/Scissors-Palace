ffa2945068bbd190942b38a39b562c4a2a5bd3bb
Celeste Moore <celestealexmoore@gmail.com>
2021-08-08 16:10:18 -0500

    adding photos to reviews

--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,72 +1,123 @@
-import React from 'react';
+import React from "react";
 
 function Reviews() {
-  return(
+  return (
     <>
       <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
           <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
-              <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-            </svg>
-            <p class="leading-relaxed text-lg">Jesse is great! Thoughtful and careful, he doesn’t whip through the haircut. Spends time with his clients and will make take time making adjustments. No rush, great style, trust him with your head!</p>
+            <img
+              src="./images/rock.png"
+              class="inline-block w-18 h-18 text-gray-400 mb-8"
+            ></img>
+            <p class="leading-relaxed text-lg">
+              Jesse is great! Thoughtful and careful, he doesn’t whip through
+              the haircut. Spends time with his clients and will make take time
+              making adjustments. No rush, great style, trust him with your
+              head!
+            </p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Dwane 'The Rock' Johnson</h2>
+            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+              Dwane 'The Rock' Johnson
+            </h2>
             <p class="text-gray-500">American Actor</p>
           </div>
         </div>
+
         <div class="container px-5 py-24 mx-auto">
           <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
-              <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-            </svg>
-            <p class="leading-relaxed text-lg">I’ve been seeing Ida here for my color for the last few years. No matter where I move in the city, I still go to her. The salon is friendly and cozy, and who can complain about a glass of wine while your color is sitting?</p>
+            <img
+              src="./images/yonce.png"
+              class="inline-block w-18 h-18 text-gray-400 mb-8"
+            ></img>
+            <p class="leading-relaxed text-lg">
+              I’ve been seeing Ida here for my color for the last few years. No
+              matter where I move in the city, I still go to her. The salon is
+              friendly and cozy, and who can complain about a glass of wine
+              while your color is sitting?
+            </p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Beyonce Knowles-Carter</h2>
+            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+              Beyonce Knowles-Carter
+            </h2>
             <p class="text-gray-500">American Singer-Songwriter</p>
           </div>
         </div>
         <div class="container px-5 py-24 mx-auto">
           <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
-              <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-            </svg>
-            <p class="leading-relaxed text-lg">Go see Celeste! She is fantastic and saved my hair who someone else totally ruined. I did not even have to cut it. She is an absolute professional and takes care of you 100%. Her color correction was totally amazing and I had a great time too.</p>
+            <img
+              src="./images/oprah.png"
+              class="inline-block w-17 h-17 text-gray-400 mb-8"
+            ></img>
+            <p class="leading-relaxed text-lg">
+              Go see Celeste! She is fantastic and saved my hair who someone
+              else totally ruined. I did not even have to cut it. She is an
+              absolute professional and takes care of you 100%. Her color
+              correction was totally amazing and I had a great time too.
+            </p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Oprah Winfrey</h2>
+            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+              Oprah Winfrey
+            </h2>
             <p class="text-gray-500">American Host</p>
           </div>
         </div>
         <div class="container px-5 py-24 mx-auto">
           <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
-              <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-            </svg>
-            <p class="leading-relaxed text-lg">Nobody cuts hair like Kevin. Nobody. If he gets any kindler or gentler, he's literally going to ceast to exist. I will build a great great momument of him and keep all the bad stylists out of the country. And they will pay for that monument. Mark my words. I have a great relationship with him, as you possibly have heard. And he likes me. I like him. People vote for me because I'm so good looking and Kevin makes sure of it. I have the best haircut to ever come out of a salon, by far. Nobody has a better haircut than me. I have the best one. I look very much forward to showing my haircut. Nobody writes better reviews than me. I've got a piece of pocket-lint worth more than Beyonce's review.</p>
+            <img
+              src="./images/trump.png"
+              class="inline-block w-18 h-18 text-gray-400 mb-8"
+            ></img>
+            <p class="leading-relaxed text-lg">
+              Nobody cuts hair like Kevin. Nobody. If he gets any kindler or
+              gentler, he's literally going to ceast to exist. I will build a
+              great great momument of him and keep all the bad stylists out of
+              the country. And they will pay for that monument. Mark my words. I
+              have a great relationship with him, as you possibly have heard.
+              And he likes me. I like him. People vote for me because I'm so
+              good looking and Kevin makes sure of it. I have the best haircut
+              to ever come out of a salon, by far. Nobody has a better haircut
+              than me. I have the best one. I look very much forward to showing
+              my haircut. Nobody writes better reviews than me. I've got a piece
+              of pocket-lint worth more than Beyonce's review.
+            </p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Donald Trump</h2>
+            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+              Donald Trump
+            </h2>
             <p class="text-gray-500">Former U.S. President</p>
           </div>
         </div>
         <div class="container px-5 py-24 mx-auto">
           <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
-              <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
-            </svg>
-            <p class="leading-relaxed text-lg">Erick is my go to person when I getting my hair cut in Houston. I always struggle between what I want and what I need. Erick finds really good balance and gives the best advice every time!</p>
+            <img
+              src="./images/bezos.png"
+              class="inline-block w-18 h-18 text-gray-400 mb-8"
+            ></img>
+            <p class="leading-relaxed text-lg">
+              Erick is my go to person when I getting my hair cut in Houston. I
+              always struggle between what I want and what I need. Erick finds
+              really good balance and gives the best advice every time!
+            </p>
             <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6"></span>
-            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Jeff Bezos</h2>
+            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
+              Jeff Bezos
+            </h2>
             <p class="text-gray-500">American Business Magnate</p>
           </div>
         </div>
       </section>
-
     </>
-  )
+  );
 }
 
 export default Reviews;
 
 <div class="sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-<img src="./images/partHighlight.png" alt="Display Picture of partial highlight" role="img" className="rounded-full object-cover h-full w-full shadow-md"></img>
-</div>
+  <img
+    src="./images/partHighlight.png"
+    alt="Display Picture of partial highlight"
+    role="img"
+    className="rounded-full object-cover h-full w-full shadow-md"
+  ></img>
+</div>;
