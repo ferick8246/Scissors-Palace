4a7c9b572353550619225e406667a5da4532b3b7
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-08-08 14:12:41 -0500

    finished footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebookSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
-    <footer className="w-100 p-4">
-      <div className="text-center mb-5">
-        <h4>
+    <footer className="bg-gray-900 w-100 p-4">
+      <div className="text-center text-white">
+        <h4 className="mb-4 text-yellow-100">
           Made with{' '}
           <span
             className="emoji"
@@ -15,7 +17,42 @@ const Footer = () => {
             ❤️
           </span>{' '}
           by Team Mongoose.
+          <br/>
         </h4>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-items-center">
+          <div className="">
+            <iframe
+              width="250px"
+              height="250px"
+              title="map"
+              className="inset-0"
+              frameBorder={0}
+              marginHeight={0}
+              marginWidth={0}
+              style={{ filter: "opacity(0.7)" }}
+              src="https://www.google.com/maps/embed/v1/place?q=Cutloose+Houston,+Texas&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            />
+          </div>
+          <div>
+            <h3 className="font-bold text-lg">Hours of Operation</h3>
+            <br/>
+            <h4 className="font-semibold">Tuesday - Thursday:</h4> <p>10:00 - 19:00</p>
+            <br/>
+            <h4 className="font-semibold">Friday & Saturday:</h4> <p>11:00 - 20:00</p>
+            <br/>
+            <h4 className="font-semibold">Closed Sundays</h4>
+          </div>
+          <div className="space-y-5">
+            <h3 className="font-bold text-lg">Contact:</h3>
+            <a href="tel:+12813308004">(281) 330-8004</a>
+            <br/>
+            <div className="space-x-5">
+            <a href="http://instagram.com/"><FontAwesomeIcon icon={faInstagram} size="lg"/></a>
+            <a href="http://facebook.com/"><FontAwesomeIcon icon={faFacebookSquare} size="lg"/></a>
+            <a href="http://twitter.com/"><FontAwesomeIcon icon={faTwitterSquare} size="lg" /></a>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
