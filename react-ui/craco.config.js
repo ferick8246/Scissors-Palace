ad32ba845e7557ed2f9503edd1e0be61d83c697f
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-08-09 01:08:01 -0500

    added cracoless

--- react-ui/craco.config.js
+++ react-ui/craco.config.js
@@ -1,9 +1,21 @@
+const CracoLessPlugin = require('craco-less');
+
 module.exports = {
     style: {
       postcss: {
         plugins: [
           require('tailwindcss'),
           require('autoprefixer'),
+          {
+            plugin: CracoLessPlugin,
+            options: {
+              lessLoaderOptions: {
+                lessOptions: {
+                  javascriptEnaled: true
+                }
+              }
+            }
+          }
         ],
       },
     },
