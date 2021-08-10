d2dcf9d0ef9e431a573c91f6dd6a69ef8e94ea34
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-08-10 13:23:46 -0500

    polished up elements to score better in lighthouse

--- react-ui/src/components/layout/Header.js
+++ react-ui/src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(){
     return(
         <div className="grid justify-items-center pt-32 md:pt-12">
-            <img className="transform w-1/3 md:scale-50 lg:scale-75" src="./images/logo.png"/>
+            <img className="transform w-1/3 md:scale-50 lg:scale-75" src="./images/logo.png" alt="image of logo"/>
         </div>
     )
 }
