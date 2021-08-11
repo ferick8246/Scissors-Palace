3a0c9916b0ded8c1c49b7b70bf4b21218131d3bf
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-08-10 19:44:57 -0500

    add third photo w/ description

--- react-ui/src/components/Gallery.js
+++ react-ui/src/components/Gallery.js
@@ -56,6 +56,28 @@ function Gallery() {
           </div>
         </div>
 
+        <div class="flex flex-wrap -m-4">
+          <div class="lg:w-1/3 sm:w-1/2 p-4">
+            <div class="flex relative">
+              <img
+                alt="gallery"
+                class="absolute inset-0 w-full h-full object-cover object-center"
+                src="./images/haircut-4.jpg"
+              ></img>
+              <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">
+                THE RACHELLE
+                </h2>
+                {/* <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  Shooting Stars
+                </h1> */}
+                <p class="leading-relaxed">
+                ... And just to let you know... we were on a break!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
         
 
       </div>
