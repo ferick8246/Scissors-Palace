f90ad061e883dd6bd65bfcc2275142347967894e
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-08-10 20:12:12 -0500

    add eighth photo w/ description

--- react-ui/src/components/Gallery.js
+++ react-ui/src/components/Gallery.js
@@ -171,7 +171,29 @@ function Gallery() {
             </div>
           </div>
         </div>
-        
+
+        <div class="flex flex-wrap -m-4">
+          <div class="lg:w-1/3 sm:w-1/2 p-4">
+            <div class="flex relative">
+              <img
+                alt="gallery"
+                class="absolute inset-0 w-full h-full object-cover object-center"
+                src="./images/Mr-T.jpg"
+              ></img>
+              <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">
+                THE MR. T
+                </h2>
+                {/* <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  Shooting Stars
+                </h1> */}
+                <p class="leading-relaxed">
+                "I pitty the fool"
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
 
       </div>
     </section>
