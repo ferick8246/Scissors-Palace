c184f2eb6560bec9583e0ab0ebb1080b6699c0b0
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-08-10 22:12:39 -0500

    button

--- react-ui/src/components/layout/Cart.js
+++ react-ui/src/components/layout/Cart.js
@@ -91,7 +91,7 @@ const Cart = () => {
             <strong>Total: ${calculateTotal()}</strong>
 
             {Auth.loggedIn() ? (
-              <button onClick={submitCheckout}>Checkout</button>
+              <button onClick={submitCheckout} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">Checkout</button>
             ) : (
               <span><br/>(log in to check out)</span>
             )}
