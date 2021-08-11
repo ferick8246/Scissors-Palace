3648fd47661110160b19ffc369e0cb600860c4a7
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-08-10 19:37:26 -0500

    add second photo w/ description

--- react-ui/src/components/Gallery.js
+++ react-ui/src/components/Gallery.js
@@ -20,19 +20,44 @@ function Gallery() {
               ></img>
               <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                 <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">
-                  THE SUBTITLE
+                  TAPER FADE
                 </h2>
-                <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                {/* <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
                   Shooting Stars
-                </h1>
+                </h1> */}
                 <p class="leading-relaxed">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
+                Start lookin fresh with a fade that tapers
                 </p>
               </div>
             </div>
           </div>
         </div>
+
+        <div class="flex flex-wrap -m-4">
+          <div class="lg:w-1/3 sm:w-1/2 p-4">
+            <div class="flex relative">
+              <img
+                alt="gallery"
+                class="absolute inset-0 w-full h-full object-cover object-center"
+                src="./images/haircut-2.jpg"
+              ></img>
+              <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">
+                  THE BUNS
+                </h2>
+                {/* <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  Shooting Stars
+                </h1> */}
+                <p class="leading-relaxed">
+                Unless you got buns huns
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+
       </div>
     </section>
   );
