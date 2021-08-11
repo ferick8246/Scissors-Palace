706b870f51a964c855409c1959b7da8bf0f5cda5
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-08-10 21:59:08 -0500

    added success page

---
+++ react-ui/src/components/Success.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_ORDER } from '../utils/mutations';
+import { idbPromise } from '../utils/helpers';
+
+function Success() {
+  const [addOrder] = useMutation(ADD_ORDER);
+
+  useEffect(() => {
+    async function saveOrder() {
+      const cart = await idbPromise('cart', 'get');
+      const products = cart.map((item) => item._id);
+
+      if (products.length) {
+        const { data } = await addOrder({ variables: { products } });
+        const productData = data.addOrder.products;
+
+        productData.forEach((item) => {
+          idbPromise('cart', 'delete', item);
+        });
+      }
+
+      setTimeout(() => {
+        window.location.assign('/');
+      }, 3000);
+    }
+
+    saveOrder();
+  }, [addOrder]);
+
+  return (
+    <div className="container text-center">
+        <h1>Success!</h1>
+        <h2>Thank you for your purchase!</h2>
+        <h2>You will now be redirected to the home page</h2>
+    </div>
+  );
+}
+
+export default Success;
