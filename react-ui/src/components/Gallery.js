9d96d4db3fdfe92493211f26af0e2189dd4f540c
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-08-10 19:54:56 -0500

    add fifth photo w/ description

--- react-ui/src/components/Gallery.js
+++ react-ui/src/components/Gallery.js
@@ -72,7 +72,7 @@ function Gallery() {
                   Shooting Stars
                 </h1> */}
                 <p class="leading-relaxed">
-                ... And just to let you know... we were on a break!
+                "... And just to let you know... we were on a break!"
                 </p>
               </div>
             </div>
@@ -95,7 +95,31 @@ function Gallery() {
                   Shooting Stars
                 </h1> */}
                 <p class="leading-relaxed">
-                May I speak to your manager?
+                "May I speak to your manager?"
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex flex-wrap -m-4">
+          <div class="lg:w-1/3 sm:w-1/2 p-4">
+            <div class="flex relative">
+              <img
+                alt="gallery"
+                class="absolute inset-0 w-full h-full object-cover object-center"
+                src="./images/haircut-6.jpg"
+              ></img>
+              <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 class="tracking-widest text-sm title-font font-medium text-yellow-500 mb-1">
+                THE KRAVITZ
+                </h2>
+                {/* <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                  Shooting Stars
+                </h1> */}
+                <p class="leading-relaxed">
+                "But what I really want to know is
+                Are you gonna go my way?"
                 </p>
               </div>
             </div>
